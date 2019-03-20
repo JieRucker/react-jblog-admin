73fbc808af93cf7897fc682d3de1665db24588d3
JieRucker <1435255141@qq.com>
2019-03-20 23:57:39 +0800

    [fix] Update

--- src/main.js
+++ src/main.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import reducers from './reducers';
+import {createStore, applyMiddleware, compose} from 'redux';
+import thunk from 'redux-thunk';
+import {Provider} from 'react-redux';
+import Page from './routes';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import './api/http';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const store = createStore(reducers, compose(
+  applyMiddleware(thunk), window.devToolsExtension ?
+    window.devToolsExtension() :
+    f => f
+));
+
+const render = Component => {
+  ReactDOM.render((
+    <Provider store={store}>
+      <Component store={store}></Component>
+    </Provider>
+  ), document.getElementById('root'))
+};
+
+render(Page);
