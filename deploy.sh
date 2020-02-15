0e685a8080e55be8b72dda8954ba370344c6e481
JieRucker <1435255141@qq.com>
2020-02-15 15:31:22 +0800

    Merge branch 'master' of https://github.com/JieRucker/react-jblog-admin
    # Conflicts:
    #	src/redux/reducers.js
    #	src/views/article/new.js

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-#rm -rf /home/git/blog/work/react-jblog-admin/dist/
 docker-compose down
 docker-compose build
 docker-compose up -d
-docker cp react-jblog-admin_container:/home/react-jblog-admin/dist/ /home/git/blog/work/react-jblog-admin/dist/
+docker cp react-jblog-admin_container:/home/react-jblog-admin/dist/ /home/git/blog/work/react-jblog-admin/
 docker cp /home/git/blog/work/react-jblog-admin/dist/ nginx_container:/usr/share/nginx/html/react-jblog-admin/
 
 echo "构建成功"
